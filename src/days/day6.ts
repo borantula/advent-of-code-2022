f44e70ac67a2168374bd6d9b1f04fce8d9ed45e2
Bora Yalcin <b.yalcin@catawiki.nl>
2022-12-06 06:47:30 +0100

    day6

--- src/days/day6.ts
+++ src/days/day6.ts
@@ -1,24 +1,13 @@
-import * as ArrayFP from 'fp-ts/Array';
-import * as N from 'fp-ts/Number';
-import * as NEA from 'fp-ts/NonEmptyArray';
-import { flow, pipe } from 'fp-ts/function';
-import utils = require('../utils');
-import { sampleData, data } from './day6-data';
+import { data } from './day6-data';
 
 function getMarkerByMagicNumber(msg: string, magicNumber: number) {
-  return pipe(
-    msg,
-    (x) => {
-      const turns = x.length - magicNumber;
-      for (let index = 0; index < turns; index++) {
-        const currentSet = new Set(x.slice(index, index + magicNumber));
-        if (currentSet.size === magicNumber) {
-          return index + magicNumber;
-        }
-      }
-    },
-    (x) => x,
-  );
+  const turns = msg.length - magicNumber;
+  for (let index = 0; index < turns; index++) {
+    const currentSet = new Set(msg.slice(index, index + magicNumber));
+    if (currentSet.size === magicNumber) {
+      return index + magicNumber;
+    }
+  }
 }
 
 export function q1() {
