3879baebb4cb89e55168ed31f8f555e32608d335
Bora Yalcin <borantula@users.noreply.github.com>
2023-12-02 08:06:30 +0000

    Update day2.ts

--- src/2023/days/day2.ts
+++ src/2023/days/day2.ts
@@ -1,10 +1,8 @@
 import { sampleData, data } from './day2-data';
 import * as utils from '../../utils';
 import * as ArrayFP from 'fp-ts/Array';
-import * as N from 'fp-ts/number';
-import * as NEA from 'fp-ts/NonEmptyArray';
-import { flow, pipe } from 'fp-ts/function';
-import { sum, take } from 'lodash';
+import { pipe } from 'fp-ts/function';
+import { sum } from 'lodash';
 
 type Color = 'red' | 'blue' | 'green';
 const bounds: Record<Color, number> = {
@@ -35,13 +33,13 @@ export function q1() {
       const games = c[1].split(';').map((a) => a.trim().split(','));
       return [c[0], ...games];
     }),
-    utils.logger,
+
     ArrayFP.map((gameSet) => {
       const [gameIdentifier, ...games] = gameSet;
       const gameId = Number((gameIdentifier as string).split(' ')[1]);
       for (let i = 0; i < games.length; i++) {
         const colorSets = games[i] as string[];
-        utils.logger(colorSets);
+
         const result = playGame(colorSets);
         if (!result) {
           return false;
